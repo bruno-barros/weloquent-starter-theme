37463483e273bb1cd5cdf9c4aebcf3aff54a7c4c
Bruno Barros <brunodanca@gmail.com>
2014-12-25 23:03:13 -0200

    update notifications

--- app/autoload/admin/notifications.php
+++ app/autoload/admin/notifications.php
@@ -4,18 +4,23 @@
  */
 add_action('after_setup_theme', function ()
 {
-	/**
-	 * Remove Core updates
-	 */
-	add_filter('pre_site_transient_update_core', '__return_null');
+	if (!current_user_can('edit_users'))
+	{
+		/**
+		 * Remove Core updates
+		 */
+		add_filter('pre_option_update_core', '__return_null');
 
-	/**
-	 * remove Plugins updates
-	 */
-//	add_filter('pre_site_transient_update_plugins', '__return_null');
+		/**
+		 * remove Plugins updates
+		 */
+		add_filter('pre_site_transient_update_plugins', '__return_null');
 
-	/**
-	 * Remove Themes updates
-	 */
-	add_filter('pre_site_transient_update_themes', '__return_null');
+		/**
+		 * Remove Themes updates
+		 */
+		add_filter('pre_site_transient_update_themes', '__return_null');
+
+		add_action('init', create_function('$a', "remove_action( 'init', 'wp_version_check' );"), 2);
+	}
 });
