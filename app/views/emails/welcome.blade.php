ca15e6051b149a3a408fee13562145f8dcf337bf
Bruno Barros <brunodanca@gmail.com>
2015-01-09 23:44:02 -0200

    logs.php config file, email template example

---
+++ app/views/emails/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h2>You're Welcome</h2>
+
+<div>
+    <p>Click on this link to get back to {{ link_to('/', 'home page') }}.</p>
+</div>
+</body>
+</html>
