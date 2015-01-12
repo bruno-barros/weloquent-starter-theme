027d2cf2c75874157044a1aba7105eef9076b6ca
Bruno Barros <brunodanca@gmail.com>
2015-01-12 15:25:34 -0200

    remove duplicate closing tags

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -3,6 +3,3 @@
 <p class="text-center"><a href="https://github.com/bruno-barros/weloquent-starter-theme">Starter theme</a> made for <a href="https://github.com/bruno-barros/w.eloquent">w.eloquent</a></p>
 
 <?php wp_footer()?>
-
-</body>
-</html>
