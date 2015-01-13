b905ffa0782d1317581cdb25cf3d78a00c27d4d4
Bruno Barros <brunodanca@gmail.com>
2015-01-13 13:56:21 -0200

    adding metatags

--- app/views/partials/head.blade.php
+++ app/views/partials/head.blade.php
@@ -5,3 +5,13 @@
 * </head>
  */
 ?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="{{ assets('img/favicon.ico') }}">
+<link rel="apple-touch-icon" href="{{ assets('img/apple-touch-icon.png') }}">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
