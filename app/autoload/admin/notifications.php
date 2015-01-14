4154ddd2facc7eb04bb667b1c92fc4cb72ab54f8
Bruno Barros <brunodanca@gmail.com>
2015-01-14 01:12:09 -0200

    Using Auth to check permission

--- app/autoload/admin/notifications.php
+++ app/autoload/admin/notifications.php
@@ -2,25 +2,23 @@
 /**
  * Hide updates notifications
  */
-add_action('after_setup_theme', function ()
+
+if (!Auth::is('administrator'))
 {
-	if (!current_user_can('edit_users'))
-	{
-		/**
-		 * Remove Core updates
-		 */
-		add_filter('pre_option_update_core', '__return_null');
+	/**
+	 * Remove Core updates
+	 */
+	add_filter('pre_option_update_core', '__return_null');
 
-		/**
-		 * remove Plugins updates
-		 */
-		add_filter('pre_site_transient_update_plugins', '__return_null');
+	/**
+	 * remove Plugins updates
+	 */
+	add_filter('pre_site_transient_update_plugins', '__return_null');
 
-		/**
-		 * Remove Themes updates
-		 */
-		add_filter('pre_site_transient_update_themes', '__return_null');
+	/**
+	 * Remove Themes updates
+	 */
+	add_filter('pre_site_transient_update_themes', '__return_null');
 
-		add_action('init', create_function('$a', "remove_action( 'init', 'wp_version_check' );"), 2);
-	}
-});
+	add_action('init', create_function('$a', "remove_action( 'init', 'wp_version_check' );"), 2);
+}
