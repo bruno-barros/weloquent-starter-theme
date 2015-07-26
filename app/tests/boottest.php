f77137098918821f55eb9cfe4b204977a3521fbb
Bruno Barros <brunodanca@gmail.com>
2015-07-26 18:41:39 -0300

    remove phpunit helpers

--- app/tests/boottest.php
+++ app/tests/boottest.php
@@ -15,13 +15,3 @@ $root = __DIR__ . '/../../../../..';
  * ------------------------------------------------
  */
 require $root . '/src/bootstrap/start.php';
-
-/**
- * ------------------------------------------------
- * Load PHPUnit helpers
- * ------------------------------------------------
- */
-//if(file_exists($root . '/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php'))
-//{
-//	require_once $root . '/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
-//}
